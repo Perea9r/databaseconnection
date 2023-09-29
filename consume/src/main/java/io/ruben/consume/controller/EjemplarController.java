60543322fe03609c05c2f95fb8924e2d0e794ea9
Perea9r <ruben.perea@viewnext.com>
2023-09-29 17:56:13 +0200

    changes

--- consume/src/main/java/io/ruben/consume/controller/EjemplarController.java
+++ consume/src/main/java/io/ruben/consume/controller/EjemplarController.java
@@ -1,5 +1,23 @@
 package io.ruben.consume.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.ruben.consume.model.Ejemplar;
+import io.ruben.consume.service.EjemplarService;
+
+@RestController
 public class EjemplarController {
-    
+    @Autowired
+    EjemplarService service;
+
+    @PostMapping(value="ejemplar/{isbn}/{nombre}/{duracion}/{precio}")
+    public List<Ejemplar> altaEjemplar(@PathVariable int isbn, @PathVariable String nombre, @PathVariable int duracion, @PathVariable int precio){
+        Ejemplar ejemplar = new Ejemplar(isbn, nombre, duracion, precio);
+        return service.nuevoEjemplar(ejemplar);
+    }
 }
--- consume/src/main/java/io/ruben/consume/service/EjemplarService.java
+++ consume/src/main/java/io/ruben/consume/service/EjemplarService.java
@@ -1,5 +1,9 @@
 package io.ruben.consume.service;
 
+import java.util.List;
+
+import io.ruben.consume.model.Ejemplar;
+
 public interface EjemplarService {
-    
+        List<Ejemplar> nuevoEjemplar(Ejemplar ejemplar);
 }
